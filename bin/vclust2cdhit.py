27ce282833ea88d17fb98eb883effba6404cf8ab
Kevin Lamkiewicz <kevin.lamkiewicz@uni-jena.de>
2020-09-01 17:09:58 +0200

    added vclust2cdhit parser

---
+++ bin/vclust2cdhit.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+# Author: Kevin Lamkiewicz
+# Email: kevin.lamkiewicz@uni-jena.de
+
+"""
+"""
+
+import sys
+
+inputFile = sys.argv[1]
+outputFile = f"{sys.argv[1]}.clstr"
+
+centroids = {}
+cluster = {}
+
+with open(inputFile, 'r') as inputStream:
+  for line in inputStream:
+    lineArray = line.strip().split('\t')
+    if line.startswith('S'):
+      centroids[lineArray[1]] = (lineArray[8], lineArray[2])
+      
+    if line.startswith('H'):
+      if lineArray[1] in cluster:
+        cluster[lineArray[1]].append((lineArray[8], lineArray[2], lineArray[3]))
+      else:
+        cluster[lineArray[1]] = [(lineArray[8], lineArray[2], lineArray[3])]
+
+with open(outputFile, 'w') as outputStream:
+  for idx, content in centroids.items():
+    outputStream.write(f">Cluster {idx}\n")
+    outputStream.write(f"0\t{content[1]}nt, >{content[0]} *\n")
+    if not idx in cluster:
+      continue
+
+    for seqIdx, clustercontent in enumerate(cluster[idx]):
+      outputStream.write(f"{seqIdx+1}\t{clustercontent[1]}nt, >{clustercontent[0]} at +/{clustercontent[2]}%\n")
